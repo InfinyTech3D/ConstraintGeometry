22c6ec0edc4cbb262e04c89213534869940edd8d
omarboukhris <omarboukhris@gmail.com>
2019-04-12 11:29:48 +0200

    1st implementation of projective2dCst, compiles fine, doesn't segfault but needs debugging

---
+++ src/sofa/constraintGeometry/constraint/ConstraintProjective2D.cpp
@@ -0,0 +1,13 @@
+#include <sofa/constraintGeometry/constraint/ConstraintProjective2D.h>
+#include <sofa/core/ObjectFactory.h>
+
+namespace sofa {
+
+namespace constraintGeometry {
+
+int ConstraintProjective2DClass = core::RegisterObject("ConstraintProjective2D")
+.add< ConstraintProjective2D >();
+
+}
+
+}
