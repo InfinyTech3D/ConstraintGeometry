dbb9ee22a08389b74b54ace0bf0c79e4e9bfeae3
Claire Martin <claire.martin2@etu.unistra.fr>
2022-06-02 18:02:01 +0200

    up

--- src/sofa/constraintGeometry/ConstraintProximity.cpp
+++ src/sofa/constraintGeometry/ConstraintProximity.cpp
@@ -14,8 +14,8 @@ int phong_reg = ConstraintProximityOperation::register_func<PhongTriangleNormalH
 
 int edge_reg = ConstraintProximityOperation::register_func<EdgeNormalHandler>(&EdgeNormalHandler::buildConstraintProximity);
 
-//int gravityPoint_reg = ConstraintProximityOperation::register_func<GravityPointNormalHandler>(&GravityPointNormalHandler::buildConstraintProximity);
+int gravityPoint_reg = ConstraintProximityOperation::register_func<GravityPointNormalHandler>(&GravityPointNormalHandler::buildConstraintProximity);
 
-//int vectorPoint_reg = ConstraintProximityOperation::register_func<VectorPointNormalHandler<sofa::defaulttype::Vec3dTypes>>(&VectorPointNormalHandler<sofa::defaulttype::Vec3dTypes>::buildConstraintProximity);
+int vectorPoint_reg = ConstraintProximityOperation::register_func<VectorPointNormalHandler<sofa::defaulttype::Vec3dTypes>>(&VectorPointNormalHandler<sofa::defaulttype::Vec3dTypes>::buildConstraintProximity);
 
 }
