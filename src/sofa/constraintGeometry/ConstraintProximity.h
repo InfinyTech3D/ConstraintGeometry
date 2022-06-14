c199bcb2832c6d93f63a603bab91a5674a726c8d
Claire Martin <claire.martin2@etu.unistra.fr>
2022-06-14 10:55:03 +0200

    Creation of a MultiConstraintProximity

--- src/sofa/constraintGeometry/ConstraintProximity.h
+++ src/sofa/constraintGeometry/ConstraintProximity.h
@@ -83,6 +83,8 @@ public:
 class DefaultConstraintProximity : public ConstraintProximity {
 public:
 
+    typedef std::shared_ptr<DefaultConstraintProximity> SPtr;
+
     DefaultConstraintProximity(const collisionAlgorithm::BaseProximity::SPtr & p)
     : m_prox(p) {}
 
@@ -108,6 +110,48 @@ public:
 
 protected:
     collisionAlgorithm::BaseProximity::SPtr m_prox;
+};
+
+
+
+class MultiConstraintProximity : public ConstraintProximity {
+public:
+    typedef std::shared_ptr<MultiConstraintProximity> SPtr;
+
+    MultiConstraintProximity(const std::vector<ConstraintProximity::SPtr> & p)
+    : m_proximities(p) {}
+
+    sofa::type::Vector3 getPosition(core::VecCoordId v = core::VecCoordId::position()) const override {
+        sofa::type::Vector3 P(0,0,0);
+        for (unsigned i=0; i<m_proximities.size(); i++) P+=m_proximities[i]->getPosition(v);
+        return P*1.0/m_proximities.size();
+    }
+
+    void buildJacobianConstraint(core::MultiMatrixDerivId cId, const sofa::type::vector<sofa::type::Vector3> & dir, double fact, Index constraintId) const override {
+        fact *= 1.0/m_proximities.size();
+        for (unsigned i=0; i<m_proximities.size(); i++) {
+            m_proximities[i]->buildJacobianConstraint(cId,dir,fact,constraintId);
+        }
+    }
+
+    void storeLambda(const core::ConstraintParams* cParams, core::MultiVecDerivId resId, Index cid_global, Index cid_local, const sofa::linearalgebra::BaseVector* lambda) const override {
+        for (unsigned i=0; i<m_proximities.size(); i++) {
+            m_proximities[i]->storeLambda(cParams,resId,cid_global,cid_local,lambda);
+        }
+    }
+
+    type::Vector3 getNormal() const override {
+        sofa::type::Vector3 N(0,0,0);
+        for (unsigned i=0; i<m_proximities.size(); i++) N+=m_proximities[i]->getNormal();
+        return N*1.0/m_proximities.size();
+    }
+
+    static ConstraintProximity::SPtr create(const std::vector<ConstraintProximity::SPtr> & p) {
+        return ConstraintProximity::SPtr(new MultiConstraintProximity(p));
+    }
+
+protected:
+    std::vector<ConstraintProximity::SPtr> m_proximities;
 };
 
 }
