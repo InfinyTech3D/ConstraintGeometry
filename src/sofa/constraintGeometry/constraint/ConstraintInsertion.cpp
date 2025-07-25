532df508bbe30021eee60879997da1b5cbdd6980
Themis Skamagkis <70031729+th-skam@users.noreply.github.com>
2025-07-25 16:02:48 +0200

    [Needle][constraint] Addition of two bilateral constraints on the needle shaft during the insertion (#6)
    * The implementation is in the ConstraintInsertion class and relies on the use of the EdgeNormalHandler and the FirstDirection classes.

---
+++ src/sofa/constraintGeometry/constraint/ConstraintInsertion.cpp
@@ -0,0 +1,14 @@
+#include <sofa/constraintGeometry/constraint/ConstraintInsertion.h>
+#include <sofa/core/ObjectFactory.h>
+
+namespace sofa {
+
+namespace constraintGeometry {
+
+int ConstraintInsertionClass = core::RegisterObject("ConstraintInsertion")
+.add< ConstraintInsertion >();
+
+}
+
+}
+
