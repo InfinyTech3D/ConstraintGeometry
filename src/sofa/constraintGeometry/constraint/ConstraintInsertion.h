dd7aa40ff0fd038ff94493f7163b1fc77496bbed
Themis Skamagkis <70031729+th-skam@users.noreply.github.com>
2025-07-28 13:25:40 +0200

    [Needle][constraint] Addition of the frictional constraint (#9)

--- src/sofa/constraintGeometry/constraint/ConstraintInsertion.h
+++ src/sofa/constraintGeometry/constraint/ConstraintInsertion.h
@@ -10,12 +10,14 @@ class ConstraintInsertion : public TBaseConstraint<collisionAlgorithm::BaseProxi
 public:
     SOFA_CLASS(ConstraintInsertion , SOFA_TEMPLATE2(TBaseConstraint,BaseProximity,BaseProximity));
 
+    Data<SReal> d_frictionCoeff;
     Data<sofa::type::vector<double> > d_maxForce;
     Data<sofa::type::vector<double> > d_compliance;
     core::objectmodel::SingleLink<ConstraintInsertion,ConstraintDirection, BaseLink::FLAG_STRONGLINK|BaseLink::FLAG_STOREPATH> l_directions;
 
     ConstraintInsertion()
-    : d_maxForce(initData(&d_maxForce, "maxForce", "Max force"))
+    : d_frictionCoeff(initData(&d_frictionCoeff, 0.0, "frictionCoeff" , "static friction coefficient (should be less than 1)"))
+    , d_maxForce(initData(&d_maxForce, "maxForce", "Max force"))
     , d_compliance(initData(&d_compliance, "compliance", "Max force"))
     , l_directions(initLink("directions", "link to the default direction")) {}
 
@@ -27,44 +29,23 @@ public:
 
     virtual ConstraintNormal createConstraintNormal(const BaseProximity::SPtr & first, const BaseProximity::SPtr & second) const override {
         if (l_directions == NULL) return ConstraintNormal();
-        ConstraintNormal cst(l_directions->createConstraintsNormal(first,second));
-        cst.addOrthogonalDirection().addOrthogonalDirection();
-        return ConstraintNormal().add(cst[1]).add(cst[2]);// l_directions->createConstraintsNormal(first, second);
+        return l_directions->createConstraintsNormal(first, second)
+            .addOrthogonalDirection().addOrthogonalDirection();
     }
 
     core::behavior::ConstraintResolution* createConstraintResolution(const BaseInternalConstraint * cst) const {
-        if (cst->size() == 1) {
-            double maxf = std::numeric_limits<double>::max();
-            double comp = 0.0;
-            if (d_maxForce.getValue().size()>0) maxf=d_maxForce.getValue()[0];
-            if (d_compliance.getValue().size()>0) comp=d_compliance.getValue()[0];
-            return new InsertionConstraintResolution1(maxf,comp);
-        } else if (cst->size() == 2) {
-            double maxf[2] = { std::numeric_limits<double>::max(), std::numeric_limits<double>::max()};
-            double comp[2] = { 0.0, 0.0 };
+        double maxf[3] = { std::numeric_limits<double>::max(), std::numeric_limits<double>::max(), std::numeric_limits<double>::max()};
+        double comp[3] = { 0.0, 0.0, 0.0 };
 
-            if (d_maxForce.getValue().size()>0) maxf[0]=d_maxForce.getValue()[0];
-            if (d_maxForce.getValue().size()>1) maxf[1]=d_maxForce.getValue()[1];
+        if (d_maxForce.getValue().size()>0) maxf[0]=d_maxForce.getValue()[0];
+        if (d_maxForce.getValue().size()>1) maxf[1]=d_maxForce.getValue()[1];
+        if (d_maxForce.getValue().size()>2) maxf[2]=d_maxForce.getValue()[2];
 
-            if (d_compliance.getValue().size()>0) comp[0]=d_compliance.getValue()[0];
-            if (d_compliance.getValue().size()>1) comp[1]=d_compliance.getValue()[1];
+        if (d_compliance.getValue().size()>0) comp[0]=d_compliance.getValue()[0];
+        if (d_compliance.getValue().size()>1) comp[1]=d_compliance.getValue()[1];
+        if (d_compliance.getValue().size()>2) comp[2]=d_compliance.getValue()[2];
 
-            return new InsertionConstraintResolution2(maxf[0],maxf[1],comp[0],comp[1]);
-        } else if (cst->size() == 3) {
-
-            double maxf[3] = { std::numeric_limits<double>::max(), std::numeric_limits<double>::max(), std::numeric_limits<double>::max()};
-            double comp[3] = { 0.0, 0.0, 0.0 };
-
-            if (d_maxForce.getValue().size()>0) maxf[0]=d_maxForce.getValue()[0];
-            if (d_maxForce.getValue().size()>1) maxf[1]=d_maxForce.getValue()[1];
-            if (d_maxForce.getValue().size()>2) maxf[2]=d_maxForce.getValue()[2];
-
-            if (d_compliance.getValue().size()>0) comp[0]=d_compliance.getValue()[0];
-            if (d_compliance.getValue().size()>1) comp[1]=d_compliance.getValue()[1];
-            if (d_compliance.getValue().size()>2) comp[2]=d_compliance.getValue()[2];
-
-            return new InsertionConstraintResolution3(maxf[0],maxf[1],maxf[2],comp[0],comp[1],comp[2]);
-        }
+        return new InsertionConstraintResolution(d_frictionCoeff.getValue(), maxf[0],maxf[1],maxf[2],comp[0],comp[1],comp[2]);
 
         std::cerr << "Error the size of the constraint is not correct in ConstraintInsertion size=" << cst->size() << std::endl;
         return NULL;
--- src/sofa/constraintGeometry/constraint/InsertionResolution.h
+++ src/sofa/constraintGeometry/constraint/InsertionResolution.h
@@ -8,71 +8,9 @@ namespace sofa {
 
 namespace constraintGeometry {
 
-class InsertionConstraintResolution1 : public sofa::core::behavior::ConstraintResolution {
+class InsertionConstraintResolution : public sofa::core::behavior::ConstraintResolution {
 public:
-    InsertionConstraintResolution1(double maxf1 = std::numeric_limits<double>::max(), double comp1 = 0.0) : sofa::core::behavior::ConstraintResolution(1) {
-        m_maxForce = maxf1;
-
-        m_compliance = comp1;
-    }
-
-    virtual void resolution(int line, double** w, double* d, double* force, double * /*dFree*/) {
-        force[line] -= d[line] / (w[line][line] + m_compliance);
-        if (force[line] > m_maxForce) force[line]=m_maxForce;
-        if (force[line] < -m_maxForce) force[line]=-m_maxForce;
-    }
-
-    double m_compliance;
-    double m_maxForce;
-};
-
-class InsertionConstraintResolution2 : public sofa::core::behavior::ConstraintResolution {
-public:
-    InsertionConstraintResolution2(double maxf1 = std::numeric_limits<double>::max(),double maxf2 = std::numeric_limits<double>::max(),
-                                   double comp1 = 0.0,double comp2 = 0.0) : sofa::core::behavior::ConstraintResolution(2) {
-        m_maxForce[0] = maxf1;
-        m_maxForce[1] = maxf2;
-
-        m_compliance[0] = comp1;
-        m_compliance[1] = comp2;
-    }
-
-    virtual void init(int line, double** w, double * /*force*/)
-    {
-        sofa::type::Mat<2,2,double> temp;
-        for (unsigned j=0;j<2;j++) {
-            for (unsigned i=0;i<2;i++) {
-                temp[j][i] = w[line+j][line+i];
-
-                if (i == j) temp[j][i]+=m_compliance[i];
-            }
-        }
-
-        SOFA_UNUSED(sofa::type::invertMatrix(invW,temp));
-    }
-
-    virtual void resolution(int line, double** /*w*/, double* d, double* force, double * /*dFree*/)
-    {
-        for(int i=0; i<2; i++) {
-            for(int j=0; j<2; j++)
-                force[line+i] -= d[line+j] * invW[i][j];
-        }
-
-        for(int i=0; i<2; i++) {
-            if (force[line+i] > m_maxForce[i]) force[line+i]=m_maxForce[i];
-            else if (force[line+i] < -m_maxForce[i]) force[line+i]=-m_maxForce[i];
-        }
-    }
-
-    double m_maxForce[2];
-    double m_compliance[2];
-    sofa::type::Mat<2,2,double> invW;
-};
-
-
-class InsertionConstraintResolution3 : public sofa::core::behavior::ConstraintResolution {
-public:
-    InsertionConstraintResolution3(double maxf1 = std::numeric_limits<double>::max(),double maxf2 = std::numeric_limits<double>::max(),double maxf3 = std::numeric_limits<double>::max(),
+    InsertionConstraintResolution(SReal frictionCoeff, double maxf1 = std::numeric_limits<double>::max(),double maxf2 = std::numeric_limits<double>::max(),double maxf3 = std::numeric_limits<double>::max(),
                                    double comp1 = 0.0,double comp2 = 0.0,double comp3 = 0.0) : sofa::core::behavior::ConstraintResolution(3) {
         m_maxForce[0] = maxf1;
         m_maxForce[1] = maxf2;
@@ -81,6 +19,8 @@ public:
         m_compliance[0] = comp1;
         m_compliance[1] = comp2;
         m_compliance[2] = comp3;
+
+        m_frictionCoeff = frictionCoeff;
     }
 
     virtual void init(int line, double** w, double * /*force*/)
@@ -97,22 +37,39 @@ public:
         SOFA_UNUSED(sofa::type::invertMatrix(invW,temp));
     }
 
-    virtual void resolution(int line, double** /*w*/, double* d, double* force, double * /*dFree*/)
+    virtual void resolution(int line, double** w, double* d, double* force, double * /*dFree*/)
     {
-        for(int i=0; i<3; i++) {
-            for(int j=0; j<3; j++)
-                force[line+i] -= d[line+j] * invW[i][j];
-        }
+        std::array<SReal, 3> corr = { 0.0, 0.0, 0.0 };
 
-        for(int i=0; i<3; i++) {
+        for (int j = 0; j < 3; j++)
+            corr[0] -= invW[0][j] * d[line+j];
+        corr[0] *= m_frictionCoeff;
+
+        // Back solve 2x2 system with friction on the RHS
+        const SReal denom = w[1][1] * w[2][2] - w[1][2] * w[2][1];
+        const SReal nom1 = w[1][2] * (d[line+2] + w[2][0] * corr[0]);
+        const SReal nom2 = w[2][2] * (d[line+1] + w[1][0] * corr[0]);
+        corr[1] = (nom1 - nom2) / denom;
+
+        corr[2] = -(d[line+2] + w[2][0] * corr[0] + w[2][1] * corr[1]) / w[2][2];
+
+        for (int i = 0; i < 3; i++) 
+            force[line+i] += corr[i];
+
+        for(int i = 0; i < 3; i++) {
             if (force[line+i] > m_maxForce[i]) force[line+i]=m_maxForce[i];
             else if (force[line+i] < -m_maxForce[i]) force[line+i]=-m_maxForce[i];
         }
+
+        //SReal slip_force = m_frictionCoeff;
+        //if (force[line+2] >  slip_force) force[line+2] = slip_force;
+        //else if (force[line+2] < -slip_force) force[line+2] = -slip_force;
     }
 
     double m_maxForce[3];
     double m_compliance[3];
     sofa::type::Mat<3,3,double> invW;
+    SReal m_frictionCoeff;
 };
 
 }
