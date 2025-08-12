082d82c9fa0b490038181e459aaa41ee3b8be956
Themis Skamagkis <themisskamagkis+github@gmail.com>
2025-08-12 22:55:14 +0200

    [src][constraint] Apply new factory registration mechanism

--- src/sofa/constraintGeometry/constraint/ConstraintBilateral.cpp
+++ src/sofa/constraintGeometry/constraint/ConstraintBilateral.cpp
@@ -1,14 +1,12 @@
 #include <sofa/constraintGeometry/constraint/ConstraintBilateral.h>
 #include <sofa/core/ObjectFactory.h>
 
-namespace sofa {
-
-namespace constraintGeometry {
-
-int ConstraintBilateralClass = core::RegisterObject("ConstraintBilateral")
-.add< ConstraintBilateral >();
-
-}
-
-}
-
+namespace sofa::constraintGeometry
+{
+    void registerConstraintBilateral(sofa::core::ObjectFactory* factory)
+    {
+        factory->registerObjects(
+            sofa::core::ObjectRegistrationData("Implements bilateral constraints between an origin and a destination geometry")
+            .add<ConstraintBilateral>());
+    }
+}  // namespace sofa::constraintGeometry
