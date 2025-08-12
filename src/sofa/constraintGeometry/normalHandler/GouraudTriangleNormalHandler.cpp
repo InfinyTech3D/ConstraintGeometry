306e765960d5ec04a222b503ff93e0769b9eab6c
Themis Skamagkis <themisskamagkis+github@gmail.com>
2025-08-12 23:34:23 +0200

    [src][normalHandler] Apply new factory registration mechanism

--- src/sofa/constraintGeometry/normalHandler/GouraudTriangleNormalHandler.cpp
+++ src/sofa/constraintGeometry/normalHandler/GouraudTriangleNormalHandler.cpp
@@ -1,17 +1,25 @@
 #include <sofa/constraintGeometry/normalHandler/GouraudTriangleNormalHandler.h>
-#include <sofa/core/ObjectFactory.h>
 #include <sofa/constraintGeometry/operations/ConstraintProximityOperation.h>
+#include <sofa/core/ObjectFactory.h>
 
-namespace sofa::constraintGeometry {
+namespace sofa::constraintGeometry
+{
 
-int GouraudTriangleNormalHandlerClass = core::RegisterObject("GouraudTriangleNormalHandler")
-.add< GouraudTriangleNormalHandler>();
-
-int gouraud_reg_tri = ConstraintProximityOperation::register_func<GouraudTriangleNormalHandler,collisionAlgorithm::TriangleProximity>
-(&GouraudTriangleNormalHandler::buildCstProximity<collisionAlgorithm::TriangleProximity>);
-
-int gouraud_reg_mech = ConstraintProximityOperation::register_func<GouraudTriangleNormalHandler,collisionAlgorithm::MechanicalProximity<sofa::defaulttype::Vec3dTypes> >
-(&GouraudTriangleNormalHandler::buildCstProximity<collisionAlgorithm::MechanicalProximity<sofa::defaulttype::Vec3dTypes>>);
-
-
+void registerGouraudTriangleNormalHandler(sofa::core::ObjectFactory* factory)
+{
+    factory->registerObjects(
+        sofa::core::ObjectRegistrationData("").add<GouraudTriangleNormalHandler>());
 }
+
+int gouraud_reg_tri =
+    ConstraintProximityOperation::register_func<GouraudTriangleNormalHandler,
+                                                collisionAlgorithm::TriangleProximity>(
+        &GouraudTriangleNormalHandler::buildCstProximity<collisionAlgorithm::TriangleProximity>);
+
+int gouraud_reg_mech = ConstraintProximityOperation::register_func<
+    GouraudTriangleNormalHandler,
+    collisionAlgorithm::MechanicalProximity<sofa::defaulttype::Vec3dTypes>>(
+    &GouraudTriangleNormalHandler::buildCstProximity<
+        collisionAlgorithm::MechanicalProximity<sofa::defaulttype::Vec3dTypes>>);
+
+}  // namespace sofa::constraintGeometry
